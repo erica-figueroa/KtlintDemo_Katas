1faae2390e38e122cf97026eac8dff13c72febff
ericafigueroaa <ericafigueroa4450@gmail.com>
2023-05-04 12:34:56 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ktlintdemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ktlintdemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) { // Hello
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    } 
+}
